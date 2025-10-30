1939d81d7a6c5fd3325c17fd544616be96a46fa0
Guilherme <guilhermegabriel3867@gmail.com>
2025-10-29 22:00:03 -0300

    Atualização e otimização de sistema updt 1

--- cpf_validator.py
+++ cpf_validator.py
@@ -1,60 +1,71 @@
 import re
 import sys
-cpf_enviado = input('Digite um CPF: ')
-cpf_enviado = re.sub(r'[^0-9]', '', cpf_enviado)#Tudo que não está entre 0-9 essa função tira da string
+#Essa conta foi criada pela receita federal para criar um cpf válido
+#Esse código refaz a conta dos 9 primeiros digitos, se os dois ultimos baterem com o digitado pelo o usuário, o cpf é válido
 
+#Cpf enviado pelo usuário
+cpf_enviado = input('Digite um CPF: ')
+
+#Valida se oque foi enviado pelo usuário está entre 0-9, ou é um caractere inválido.
+cpf_enviado = re.sub(r'[^0-9]', '', cpf_enviado)
+
+#Termina o processo caso nao haja 11 digitos
 if len(cpf_enviado) < 11:
     print("Não é um cpf válido.")
     sys.exit()#Encerra o python   
 
+#Proibe cpfs sequenciais como 11111111111 ou 99999999999 
 entrada_e_sequencial = cpf_enviado == cpf_enviado[0] * len(cpf_enviado)
-
-if entrada_e_sequencial: #Proibe cpfs sequenciais como 11111111111 ou 99999999999 
+if entrada_e_sequencial: 
     print("Você enviou dados sequênciais")
     sys.exit() 
-    
+
+#Cria uma lista vazia    
 cpf_lista = []
 
-for unidade in cpf_enviado: 
-    try:
-        cpf_int = int(unidade)
-    except ValueError:
-        print("Não é um número")
+#Adiciona os digitos a lista
+for digito in cpf_enviado: 
+    cpf_lista.append(digito)
 
-    cpf_lista.append(cpf_int)
+#É necessário a exclusão dos dois ultimos digitos para que possamos fazer a conta com os 9 outros digitos
+#Deleta o décimo primeiro digito do cpf   
+cpf_lista.pop(10)
 
-cpf_lista_salva = cpf_lista.copy()
-cpf_lista.pop(10)   
-cpf_lista.pop(9)   
- 
+#Deleta o décimo digito do cpf 
+cpf_lista.pop(9)
+
+#Conta do décimo digito
 contagem_regressiva_1 = 10
-contagem_regressiva_2 = 11
 soma_total_digito1 = 0
-soma_total_digito2 = 0
-contagem_temporaria = 0
 
 for digito_1 in cpf_lista:
-    soma_total_digito1 += digito_1 * contagem_regressiva_1
+    soma_total_digito1 += int(digito_1) * contagem_regressiva_1
     contagem_regressiva_1 -= 1
 
 primeiro_digito = (soma_total_digito1 * 10) % 11
 
+#Se o primeiro digito foi maior que nove, ele passa a ser 0, se não, ele continua valendo ele mesmo
 primeiro_digito = 0 if primeiro_digito > 9 else primeiro_digito
 
-cpf_lista_salva.pop(10)
+#Adicionando o décimo digito a lista
+cpf_lista.append(primeiro_digito)
 
-for digito_2 in cpf_lista_salva:
-    soma_total_digito2 += digito_2 * contagem_regressiva_2
+#Conta do décimo primeiro e ultimo digito
+soma_total_digito2 = 0
+contagem_regressiva_2 = 11
+
+for digito_2 in cpf_lista:
+    soma_total_digito2 += int(digito_2) * contagem_regressiva_2
     contagem_regressiva_2 -= 1
 
 segundo_digito = (soma_total_digito2 * 10) % 11
-
 segundo_digito = 0 if segundo_digito > 9 else segundo_digito
 
+#Os nove primeiros digitos mais os dois gerados
 cpf_gerado = f"{cpf_enviado[:9]}{primeiro_digito}{segundo_digito}"
 
+#Se os ultimos dois digitos gerados forem == aos do cpf enviado, então o cpf é válido
 if cpf_enviado == cpf_gerado:
     print(f'{cpf_enviado} é válido')
 else:
     print('CPF não é válido')
-
